3b745019332e10d6497fafaff91bf4c3882e9f71
Restyled.io <commits@restyled.io>
2020-04-04 17:43:54 +0000

    Restyled by clang-format

--- public/script.js
+++ public/script.js
@@ -19,26 +19,26 @@ function appendNewDream(dream) {
 
 // fetch the initial list of dreams
 fetch("/dreams")
-  .then(response => response.json()) // parse the JSON from the server
-  .then(dreams => {
-    // remove the loading text
-    dreamsList.firstElementChild.remove();
-  
-    // iterate through every dream and add it to our page
-    dreams.forEach(appendNewDream);
-  
-    // listen for the form to be submitted and add a new dream when it is
-    dreamsForm.addEventListener("submit", event => {
-      // stop our form submission from refreshing the page
-      event.preventDefault();
+    .then(response => response.json()) // parse the JSON from the server
+    .then(dreams => {
+      // remove the loading text
+      dreamsList.firstElementChild.remove();
 
-      // get dream value and add it to the list
-      let newDream = dreamsForm.elements.dream.value;
-      dreams.push(newDream);
-      appendNewDream(newDream);
+      // iterate through every dream and add it to our page
+      dreams.forEach(appendNewDream);
 
-      // reset form
-      dreamsForm.reset();
-      dreamsForm.elements.dream.focus();
+      // listen for the form to be submitted and add a new dream when it is
+      dreamsForm.addEventListener("submit", event => {
+        // stop our form submission from refreshing the page
+        event.preventDefault();
+
+        // get dream value and add it to the list
+        let newDream = dreamsForm.elements.dream.value;
+        dreams.push(newDream);
+        appendNewDream(newDream);
+
+        // reset form
+        dreamsForm.reset();
+        dreamsForm.elements.dream.focus();
+      });
     });
-  });
