95fae2414393f502345ba56da723d74e66200752
Restyled.io <commits@restyled.io>
2020-04-04 13:57:36 +0000

    Restyled by clang-format

--- server.js
+++ server.js
@@ -2,15 +2,14 @@
 // where your node app starts
 
 // we've started you off with Express (https://expressjs.com/)
-// but feel free to use whatever libraries or frameworks you'd like through `package.json`.
+// but feel free to use whatever libraries or frameworks you'd like through
+// `package.json`.
 const express = require("express");
 const app = express();
 
 // our default array of dreams
 const dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
+  "Find and count some sheep", "Climb a really tall mountain", "Wash the dishes"
 ];
 
 // make all the files in 'public' available
@@ -18,9 +17,9 @@ const dreams = [
 app.use(express.static("public"));
 
 // https://expressjs.com/en/starter/basic-routing.html
-app.get("/", (request, response) => {
-  response.sendFile(__dirname + "/views/index.html");
-});
+app.get("/",
+        (request,
+         response) => { response.sendFile(__dirname + "/views/index.html"); });
 
 // send the default array of dreams to the webpage
 app.get("/dreams", (request, response) => {
